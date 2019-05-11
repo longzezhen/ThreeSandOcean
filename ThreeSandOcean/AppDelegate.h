fffd71c52c85d9fdc4573eb8696f828169a57571
龙泽桢 <long_zezhen@sina.com>
2019-05-11 17:56:43 +0800

    Initial Commit

---
+++ ThreeSandOcean/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThreeSandOcean
+//
+//  Created by 龙泽桢 on 2019/5/11.
+//  Copyright © 2019 tools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThreeSandOcean/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ThreeSandOcean
+//
+//  Created by 龙泽桢 on 2019/5/11.
+//  Copyright © 2019 tools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
